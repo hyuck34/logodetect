959a258d264ff0af4eef7f20f535fcce5761e043
Hyungsoo Kim <javaos74@gmail.com>
2021-11-23 19:41:38 +0900

    class에 숫자가 아닌 이름으로 추가

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from numpy.lib.utils import source
 import detect
 import uuid
-import io, tempfile, os, shutil
+import io, tempfile, os, shutil, json
 from PIL import Image, ImageFilter
 
 class Main(object):
@@ -10,6 +10,7 @@ class Main(object):
         #print("working directory" + os.getcwd())
         #for f in os.listdir("/bin/"):
         #    print( "/bin/" +f)
+        self.labels = json.load( open("class_labels.json"))["labels"]
         self.rootdir = os.getcwd()
         if not os.path.isdir(self.rootdir+"/runs/detect"):
             os.mkdir(self.rootdir +"/runs")
@@ -18,21 +19,28 @@ class Main(object):
     def predict(self, skill_input):
         bytes_buf = io.BytesIO(skill_input)
         uname = str(uuid.uuid4())
-        tempfile = uname + ".jpg"
+        tempfile = uname 
         with open(tempfile, "wb") as tempf:
             tempf.write( bytes_buf.read())
         
         im = Image.open( tempfile)
         imgsize = [ im.height, im.width]
-        print('image format={0}'.format( im.format))
+        tempfile = "{0}.{1}".format( uname, im.format).lower()
+        shutil.move( uname, tempfile)
+        
+        #print('image format={0}'.format( im.format))
         
         detect.run( weights='models/samsung_logo_1123m.pt', name=uname, save_conf=True, 
                    source=tempfile, imgsz=imgsize, device='cpu', conf_thres=0.5, 
                    project=self.rootdir+'/runs/detect', save_txt=True )
-        os.remove(uname+".jpg")
+        os.remove(tempfile)
         result = { }
         result['logos'] = []
         print(os.getcwd())
+        im2 = Image.open( self.rootdir + "/runs/detect/" +uname +"/"+ tempfile)
+        hratio = im2.width / im.width
+        vratio = im2.height / im.height
+
         # no logo detected... 
         if not os.path.exists(self.rootdir+"/runs/detect/"+uname+"/labels/" + uname + ".txt"):
             result["message"] = "nothing is detected"
@@ -42,14 +50,16 @@ class Main(object):
             for l in outf.readlines():
                 obj = {}
                 items = l.split()
-                obj["class"] = items[0]
+                obj["class"] = self.labels[ int(items[0])]
                 obj["confidence"] = float(items[5])
-                obj["x"] = int(im.width * float(items[1]))
-                obj["y"] = int(im.height * float(items[2]))
-                obj["width"] = int(im.width * float(items[3]))
-                obj["height"] = int(im.height * float(items[4]))
+                obj["x"] = int(im2.width * float(items[1]))
+                obj["y"] = int(im2.height * float(items[2]))
+                obj["width"] = int(im2.width * float(items[3]))
+                obj["height"] = int(im2.height * float(items[4]))
                 result["logos"].append( obj)
         result["message"] = "ok"
+        result["height"] = im2.height
+        result['width'] = im2.width
         print( result)
         shutil.rmtree(self.rootdir+"/runs/detect/"+uname, ignore_errors=True)
         return result
@@ -58,5 +68,5 @@ class Main(object):
 
 if __name__ == '__main__':
 	m = Main()
-	with open("./samples/Test.png", "rb") as input_f:
+	with open("./samples/Test.jpg", "rb") as input_f:
 		print( m.predict( input_f.read()))
